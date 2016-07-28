7251ee36bc3accd4f32122bd415d73b32b267640
rubinkazan <safedupkid@googlemail.com>
2016-07-28 21:43:02 +0200

    Logic commented

--- Creative_Cooks_Assistant/CooksAssistant.java
+++ Creative_Cooks_Assistant/CooksAssistant.java
@@ -2,4 +2,42 @@ package RuneMate.Creative_Cooks_Assistant;
 
 public class CooksAssistant {
 
+    private NpcQueryBuilder getCook = Npcs.newQuery().names("Cook");
+    private StopWatch runningTime = new StopWatch();
+    private Coordinate startPosition;
+
+    /*
+    Logic:
+        pre-req:
+         - Are you logged in?
+         - Is your inventory Empty?
+     Quest:
+        Phase 1:
+            - Talk to Chef
+            - Accept Quest
+            - Take "empty pot"
+        Phase 2:
+            Milk:
+                - Walk to Cow area
+                - Interact with gate/check gate
+                - Take "bucket"
+                - Use "bucket" > "prized dairy cow"
+                - Check if "top-quality milk" exists in inventory
+            Egg:
+                - Walk to Chicken Farm
+                - Interact with gate/check gate
+                - Take "super large egg"
+                - Check if "super large egg" exists in inventory
+            Flour:
+                -
+
+
+
+
+
+     */
+
+
+
+
 }
